97ce8cca9ad7cebfeaec0789eafffbba98eef774
Ramnath <ramnath.krishnamurthy@storemate.com>
2012-11-23 19:42:25 +0530

    correcting syntax errors

--- notifications/utils.py
+++ notifications/utils.py
@@ -9,8 +9,8 @@ def id2slug(id):
     return id + 110909
 
 def setting(name, default=None):
-        """ Return setting value for given name or default value. """
-            return getattr(settings, name, default)
+    """ Return setting value for given name or default value. """
+    return getattr(settings, name, default)
 
 USER_MODEL = setting('AUTH_USER_MODEL') or \
                                 'User'
